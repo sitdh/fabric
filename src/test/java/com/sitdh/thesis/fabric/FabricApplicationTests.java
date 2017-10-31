ce4d56a641797ab563e49e0458ed3e846931db10
Sitdhibong Laokok <sitdhibong@gmail.com>
2017-10-31 18:54:51 +0700

    Rename classes

--- src/test/java/com/sitdh/thesis/fabric/FabricApplicationTests.java
+++ src/test/java/com/sitdh/thesis/fabric/FabricApplicationTests.java
@@ -1,5 +1,9 @@
 package com.sitdh.thesis.fabric;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,10 +11,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class FabricApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertThat(null, is(nullValue()));
 	}
 
 }
