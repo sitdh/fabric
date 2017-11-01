f080acf29a6f85fe6ecfcb18ff1625b2b9193246
Sitdhibong Laokok <sitdhibong@gmail.com>
2017-11-01 10:57:19 +0700

    Import configuration

--- src/main/java/com/sitdh/thesis/fabric/FabricApplication.java
+++ src/main/java/com/sitdh/thesis/fabric/FabricApplication.java
@@ -1,9 +1,15 @@
 package com.sitdh.thesis.fabric;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableAutoConfiguration
+@Import(value={FabricAppConfiguration.class, FirebaseAdminConfiguration.class})
 public class FabricApplication {
 
 	public static void main(String[] args) {
